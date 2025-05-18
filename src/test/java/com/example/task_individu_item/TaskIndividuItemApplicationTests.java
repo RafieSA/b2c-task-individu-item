8a0d127fd3dba746874291706fef281154c2d65c
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-05-18 21:28:11 +0700

    first commit with trying to connect to supabase

---
+++ src/test/java/com/example/task_individu_item/TaskIndividuItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_individu_item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskIndividuItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
